e1551012d51f4448dfbdfdaeca2ceb69ab41e4f7
Kevin Mickey <kmickey@gmail.com>
2021-01-09 16:59:10 -0800

    add API keys folder, but with last 7 digits censored out

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+# last seven digits changed to protect the innocent.
+
+# OpenWeatherMap API Key
+weather_api_key = "5d5dc3092ff990273449bf138XXXXXXX"
+
+# Google API Key
+g_key = "AIzaSyADfWVSQg_4hQ3yOjsKcRRnC2eBXXXXXXX"
